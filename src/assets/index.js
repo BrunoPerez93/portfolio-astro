203c5f8af8b97b01a2d1e8a029ab8d88e9702f88
BrunoPerez93 <brunoperez24793@gmail.com>
2024-12-12 13:18:00 -0300

    portafolio astro

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import adwise from './adwise.png'
+import workshop from './workshop.jpg'
+import coffee from './coffee.png'
+import qr from './qr.png'
+import music from './music.jpg'
+import pagination from './pagination.jpg'
+import quotes from './quotes.png'
+import worldRank from './world-rank.png'
+import menu from './menu.png'
+import github from './github-white.svg'
+import gmail from './gmail-white.svg'
+import linkedin from './linkedin-white.svg'
+import website from './website.png'
+import skills from './skills.png'
+import gif from './gif.png'
+
+
+
+export {adwise, workshop, coffee, qr, music, pagination, quotes, worldRank, menu, github, gmail, linkedin, website, skills, gif }
